aff291c3905b376a20ffb15761c6e345d7b49765
Sage Lee <sagemcklee@gmail.com>
2020-03-31 10:02:01 -0600

    Add test it can have renters

--- test/building_test.rb
+++ test/building_test.rb
@@ -18,7 +18,14 @@ class BuilingTest < Minitest::Test
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     building.add_unit(unit1)
-    building.add_unit(unit1)
+    building.add_unit(unit2)
     assert_equal [unit1, unit2], building.units
-  end 
+  end
+
+  def test_it_can_have_renters
+    building = Building.new
+    assert_equal [], building.renters
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+    assert_equal ["Aurora"], building.renters
 end
