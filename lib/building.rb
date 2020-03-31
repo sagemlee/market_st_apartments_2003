105df5dfdf7eeaa4c213ccf3e9ceff1290fff864
Sage Lee <sagemcklee@gmail.com>
2020-03-31 10:57:06 -0600

    Add testit can add more than one renter

--- lib/building.rb
+++ lib/building.rb
@@ -1,12 +1,24 @@
+require 'pry'
+require './lib/renter'
 class Building
-    attr_reader :units
+    attr_reader :units, :renters
 
   def initialize
     @units = []
+    @renters =[]
   end
 
   def add_unit(unit)
     @units << unit
-  end 
+  end
+
+  def renters
+    @units.each do |unit|
+      if unit.renter != nil
+    @renters << unit.renter.name
+  end
+  end
+    @renters
+end
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -24,8 +24,23 @@ class BuilingTest < Minitest::Test
 
   def test_it_can_have_renters
     building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    building.add_unit(unit1)
     assert_equal [], building.renters
     renter1 = Renter.new("Aurora")
     unit1.add_renter(renter1)
     assert_equal ["Aurora"], building.renters
+  end
+
+  def test_it_can_add_more_than_one_renter
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+        renter2 = Renter.new("Tim")
+    building.add_unit(unit2)
+    unit2.add_renter(renter2)
+    assert_equal ["Aurora", "Tim"], building.renters
 end
